3bf7aaa78608214f8893f8cfbd9cf297475fbc59
Carlos Valderrama <cvalderrama1991@gmail.com>
2025-04-27 21:04:42 -0400

    updated the npm packages

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,13 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const port = process.env.PORT;
-const app = (0, express_1.default)();
-// app.use();
-app.get('/', (req, res) => {
-    res.send('TypeScript Server!');
-    res.end();
+import http from 'node:http';
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello World!',
+    }));
 });
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`Server running at http://${host}:${port}`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import http from 'node:http';
 
-const port = process.env.PORT;
-const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
-// app.use();
-
-app.get('/', (req, res) => {
-  res.send('TypeScript Server!');
-  res.end();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server running at http://${host}:${port}`);
 });
